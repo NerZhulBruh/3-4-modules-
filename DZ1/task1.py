d14d662c89e895244f69cb479b4a3d6d6aa94ea6
NerZhulBruh <116255135+NerZhulBruh@users.noreply.github.com>
2023-02-08 00:33:09 +0300

    Delete DZ1 directory

--- DZ1/task1.py
+++
@@ -1,8 +0,0 @@
-import csv
-with open('data.txt', 'r') as file:
-    data = [line.strip().split(': ') for line in file]
-with open('data.csv', 'w', newline='') as file:
-    writer = csv.writer(file, delimiter=',')
-    writer.writerow(['name', 'grade'])
-    for row in data:
-        writer.writerow(row)
