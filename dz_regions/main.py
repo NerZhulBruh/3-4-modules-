8148c47548da42af8dfce21e2bbf0af42661a0ea
NerZhulBruh <116255135+NerZhulBruh@users.noreply.github.com>
2023-06-19 19:43:48 +0300

    main.py

--- dz_regions/main.py
+++ dz_regions/main.py
@@ -1,138 +1,286 @@
-import random
 import requests
-import csv
 import numpy as np
+import csv
 import matplotlib.pyplot as plt
+from collections import Counter
 
-
-def load_countries_name(n):
+def generate_random_regions(num_regions):
     url = 'https://randomuser.me/api/'
-    params = {'results': n}
+    params = {'results': num_regions}
     response = requests.get(url, params=params)
     data = response.json()['results']
-    countries = [user['location']['country'] for user in data]
-    return countries
+    regions = [user['location']['country'] for user in data]
+    return regions
+
+currency_codes = {
+    'Libya': 'LYD',
+ 'South Sudan': 'SSP',
+ 'Syria': 'SYP',
+ 'Venezuela': 'VES',
+ 'Yemen': 'YER',
+ 'Zimbabwe': 'ZWL',
+ 'United Arab Emirates': 'AED',
+ 'Afghanistan': 'AFN',
+ 'Albania': 'ALL',
+ 'Armenia': 'AMD',
+ 'Netherlands Antilles': 'ANG',
+ 'Angola': 'AOA',
+ 'Argentina': 'ARS',
+ 'Australia': 'AUD',
+ 'Aruba': 'AWG',
+ 'Azerbaijan': 'AZN',
+ 'Bosnia and Herzegovina': 'BAM',
+ 'Barbados': 'BBD',
+ 'Bangladesh': 'BDT',
+ 'Bulgaria': 'EUR',
+ 'Bahrain': 'BHD',
+ 'Burundi': 'BIF',
+ 'Bermuda': 'BMD',
+ 'Brunei': 'BND',
+ 'Bolivia': 'BOB',
+ 'Brazil': 'BRL',
+ 'Bahamas': 'BSD',
+ 'Bhutan': 'BTN',
+ 'Botswana': 'BWP',
+ 'Belarus': 'BYN',
+ 'Belize': 'BZD',
+ 'Canada': 'CAD',
+ 'Democratic Republic of the Congo': 'CDF',
+ 'Switzerland': 'CHF',
+ 'Chile': 'CLP',
+ 'China': 'CNY',
+ 'Colombia': 'COP',
+ 'Costa Rica': 'CRC',
+ 'Cuba': 'CUP',
+ 'Cape Verde': 'CVE',
+ 'Czech Republic': 'CZK',
+ 'Djibouti': 'DJF',
+ 'Denmark': 'EUR',
+ 'Dominican Republic': 'DOP',
+ 'Algeria': 'DZD',
+ 'Egypt': 'EGP',
+ 'Eritrea': 'ERN',
+'Ethiopia': 'ETB',
+ 'European Union': 'EUR',
+ 'Fiji': 'FJD',
+ 'Falkland Islands': 'FKP',
+ 'Faroe Islands': 'FOK',
+ 'United Kingdom': 'GBP',
+ 'Georgia': 'GEL',
+ 'Guernsey': 'GGP',
+ 'Ghana': 'GHS',
+ 'Gibraltar': 'GIP',
+ 'The Gambia': 'GMD',
+ 'Guinea': 'GNF',
+ 'Guatemala': 'GTQ',
+ 'Guyana': 'GYD',
+ 'Hong Kong': 'HKD',
+ 'Honduras': 'HNL',
+ 'Croatia': 'EUR',
+ 'Haiti': 'HTG',
+ 'Hungary': 'EUR',
+ 'Indonesia': 'IDR',
+ 'Israel': 'ILS',
+ 'Isle of Man': 'IMP',
+ 'India': 'INR',
+ 'Iraq': 'IQD',
+ 'Iran': 'IRR',
+ 'Iceland': 'ISK',
+ 'Jersey': 'JEP',
+ 'Jamaica': 'JMD',
+ 'Jordan': 'JOD',
+ 'Japan': 'JPY',
+ 'Kenya': 'KES',
+ 'Kyrgyzstan': 'KGS',
+ 'Cambodia': 'KHR',
+ 'Kiribati': 'KID',
+ 'Comoros': 'KMF',
+ 'South Korea': 'KRW',
+ 'Kuwait': 'KWD',
+ 'Cayman Islands': 'KYD',
+ 'Kazakhstan': 'KZT',
+ 'Laos': 'LAK',
+ 'Lebanon': 'LBP',
+ 'Sri Lanka': 'LKR',
+ 'Liberia': 'LRD',
+ 'Lesotho': 'LSL',
+ 'Morocco': 'MAD',
+ 'Moldova': 'MDL',
+ 'Madagascar': 'MGA',
+ 'North Macedonia': 'MKD',
+'Myanmar': 'MMK',
+ 'Mongolia': 'MNT',
+ 'Macau': 'MOP',
+ 'Mauritania': 'MRU',
+ 'Mauritius': 'MUR',
+ 'Maldives': 'MVR',
+ 'Malawi': 'MWK',
+ 'Mexico': 'MXN',
+ 'Malaysia': 'MYR',
+ 'Mozambique': 'MZN',
+ 'Namibia': 'NAD',
+ 'Nigeria': 'NGN',
+ 'Nicaragua': 'NIO',
+ 'Norway': 'NOK',
+ 'Nepal': 'NPR',
+ 'New Zealand': 'NZD',
+ 'Oman': 'OMR',
+ 'Panama': 'PAB',
+ 'Peru': 'PEN',
+ 'Papua New Guinea': 'PGK',
+ 'Philippines': 'PHP',
+ 'Pakistan': 'PKR',
+ 'Poland': 'EUR',
+ 'Paraguay': 'PYG',
+ 'Qatar': 'QAR',
+ 'Romania': 'EUR',
+ 'Serbia': 'RSD',
+ 'Russia': 'RUB',
+ 'Rwanda': 'RWF',
+ 'Saudi Arabia': 'SAR',
+ 'Solomon Islands': 'SBD',
+ 'Seychelles': 'SCR',
+ 'Sudan': 'SDG',
+ 'Sweden': 'EUR',
+ 'Singapore': 'SGD',
+ 'Saint Helena': 'SHP',
+ 'Sierra Leone': 'SLE',
+ 'Somalia': 'SOS',
+ 'Suriname': 'SRD',
+ 'São Tomé and Príncipe': 'STN',
+ 'Eswatini': 'SZL',
+ 'Thailand': 'THB',
+'Tajikistan': 'TJS',
+ 'Turkmenistan': 'TMT',
+ 'Tunisia': 'TND',
+ 'Tonga': 'TOP',
+ 'Turkey': 'TRY',
+ 'Trinidad and Tobago': 'TTD',
+ 'Tuvalu': 'TVD',
+ 'Taiwan': 'TWD',
+ 'Tanzania': 'TZS',
+ 'Ukraine': 'UAH',
+ 'Uganda': 'UGX',
+ 'United States': 'USD',
+ 'Uruguay': 'UYU',
+ 'Uzbekistan': 'UZS',
+ 'Vietnam': 'VND',
+ 'Vanuatu': 'VUV',
+ 'Samoa': 'WST',
+ 'CEMAC': 'XAF',
+ 'Organisation of Eastern Caribbean States': 'XCD',
+ 'International Monetary Fund': 'XDR',
+ 'CFA': 'XOF',
+ "Collectivités d'Outre-Mer": 'XPF',
+ 'South Africa': 'ZAR',
+ 'Zambia': 'ZMW',
+ 'Austria': 'EUR',
+ 'Belgium': 'EUR',
+ 'Cyprus': 'EUR',
+ 'Czechia': 'EUR',
+ 'Estonia': 'EUR',
+ 'Finland': 'EUR',
+ 'France': 'EUR',
+ 'Germany': 'EUR',
+ 'Greece': 'EUR',
+ 'Ireland': 'EUR',
+ 'Italy': 'EUR',
+ 'Latvia': 'EUR',
+ 'Lithuania': 'EUR',
+ 'Luxembourg': 'EUR',
+ 'Malta': 'EUR',
+ 'Netherlands': 'EUR',
+ 'Portugal': 'EUR',
+ 'Slovakia': 'EUR',
+ 'Slovenia': 'EUR',
+ 'Spain': 'EUR'
+}
+
+def get_currency_code(country):
+    return currency_codes.get(country, "Unknown")
+
+def get_exchange_rate():
+    response = requests.get("https://open.er-api.com/v6/latest/RUB")
+    data = response.json()
+    rates = data.get("rates", {})
+    exchange_rate = rates.get("RUB")
+    return exchange_rate
+
+def convert_to_rubles(amount, currency, exchange_rate):
+    if currency == "Unknown":
+        return "Unknown"
+    else:
+        return round(amount * exchange_rate, 2)
+
+np.random.seed(42)
+ids = np.arange(1, 51)
+sales = np.random.randint(10, 100, size=50)
+sales_regions = generate_random_regions(5)
 
 
-def generate_sales_data(num_samples, num_regions):
-    city_names = load_countries_name(num_regions)
-    sales_data = []
+sales_data = []
+for i in range(len(ids)):
+    country = sales_regions[i % len(sales_regions)] 
+    currency = get_currency_code(country)
+    exchange_rate = get_exchange_rate()
+    sales_rub = convert_to_rubles(sales[i], currency, exchange_rate)
+    sales_data.append({
+        "ID": ids[i],
+        "Sales": sales[i],
+        "Sales Region": sales_regions[i % len(sales_regions)],  
+        "Currency": currency,
+        "Sales (RUB)": sales_rub
+    })
 
-    for i in range(num_samples):
-        product_id = i + 1
-        sales = random.randint(1, 1000)
-        region = random.choice(city_names)
-
-        sales_data.append([product_id, sales, region])
-
-    return sales_data
-
-
-def write_sales_data(file_name, sales_data):
-    with open(file_name, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['id', 'sales', 'sales_region'])
-        writer.writerows(sales_data)
-
-
-num_samples = 50
-num_regions = 5
-
-sales_data = generate_sales_data(num_samples, num_regions)
-
-write_sales_data('result.csv', sales_data)
-
-total_sales = 0
-
-with open('result.csv', mode='r') as file:
-    reader = csv.reader(file)
-    next(reader)
-    for row in reader:
-        sales = int(row[1])
-        total_sales += sales
-
-print("Общая сумма продаж для всех продуктов:", total_sales)
-
-unique_regions = set()
-
-with open('result.csv', mode='r') as file:
-    reader = csv.reader(file)
-    next(reader)
-    for row in reader:
-        region = row[2]
-        unique_regions.add(region)
-
-num_unique_regions = len(unique_regions)
-print("Количество уникальных регионов продаж:", num_unique_regions)
-
-sales_per_product = []
-
-with open('result.csv', mode='r') as file:
-    reader = csv.reader(file)
-    next(reader)
-    for row in reader:
-        sales = int(row[1])
-        sales_per_product.append(sales)
-
-average_sales_per_product = np.mean(sales_per_product)
-print("Средняя сумма продаж на продукт:", average_sales_per_product)
-
-max_sales_product = ""
-max_sales_amount = 0
-
-with open('result.csv', mode='r') as file:
-    reader = csv.reader(file)
-    next(reader)
-    for row in reader:
-        product_id = row[0]
-        sales = int(row[1])
-        if sales > max_sales_amount:
-            max_sales_amount = sales
-            max_sales_product = product_id
-
-print("Продукт с наибольшей суммой продаж:", max_sales_product)
-
-sales_by_region = {}
-
-with open('result.csv', mode='r') as file:
-    reader = csv.reader(file)
-    next(reader)
-    for row in reader:
-        region = row[2]
-        sales = int(row[1])
-        if region in sales_by_region:
-            sales_by_region[region] += sales
-        else:
-            sales_by_region[region] = sales
-
-plt.pie(list(sales_by_region.values()), labels=list(sales_by_region.keys()), autopct='%1.1f%%')
-plt.title("Продажи по регионам")
-plt.axis('equal')
-plt.show()
-
-
-def convert_to_rubles(sales):
-    return sales * exchange_rate
-
-exchange_rate = 70.0
-
-data = []
-
-with open('result.csv', mode='r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        data.append(row)
-
-data_with_currency = []
-data_with_currency.append(data[0] + ['currency', 'sales_in_rubles'])
-
-for row in data[1:]:
-    sales = float(row[1])
-    sales_in_rubles = convert_to_rubles(sales)
-    row_with_currency = row + ['RUB', sales_in_rubles]
-    data_with_currency.append(row_with_currency)
-
-with open('result_with_currency.csv', mode='w', newline='') as file:
+filename = "sales_data.csv"
+with open(filename, mode="w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
-    writer.writerows(data_with_currency)
+    writer.writerow(["ID", "Sales", "Sales Region", "Currency", "Sales (RUB)"])
+    total_sales = 0  
+    unique_regions = set()  
+    sales_per_product = []  
+    top_products = Counter()  
+    for sale in sales_data:
+        writer.writerow([sale["ID"], sale["Sales"], sale["Sales Region"], sale["Currency"], sale["Sales (RUB)"]])
+        total_sales += sale["Sales"]
+        unique_regions.add(sale["Sales Region"])
+        sales_per_product.append(sale["Sales"])
+        top_products[sale["ID"]] += sale["Sales"]
+
+    writer.writerow([])  # Empty line before tasks
+    writer.writerow(["Task", "Result"])
+    writer.writerow(["Всего продаж", total_sales])
+    writer.writerow(["Уникальных регионов", len(unique_regions)])
+    writer.writerow(["Средние продажи", np.mean(sales_per_product)])
+    writer.writerow(["Самый дорогой продукт", top_products.most_common(1)[0][0]])
+    writer.writerow([]) 
+    writer.writerow(["Продажи по регионам"])
+    region_sales = Counter()
+    for sale in sales_data:
+        region_sales[sale["Sales Region"]] += sale["Sales"]
+    for region, sales in region_sales.items():
+        writer.writerow([region, sales])
+
+
+plt.figure(figsize=(8, 8))
+regions, sales = zip(*region_sales.items())
+plt.pie(sales, labels=regions, autopct="%1.1f%%")
+plt.title("Sales per Region")
+plt.savefig("sales_per_region.png")
+plt.close()
+
+top_products = top_products.most_common(5)
+top_products_names = [f"Product {product[0]}" for product in top_products]
+top_products_sales = [product[1] for product in top_products]
+other_sales = total_sales - sum(top_products_sales)
+top_products_names.append("Other")
+top_products_sales.append(other_sales)
+
+plt.figure(figsize=(8, 8))
+plt.pie(top_products_sales, labels=top_products_names, autopct="%1.1f%%")
+plt.title("Top 5 Products by Sales")
+plt.savefig("top_5_products.png")
+plt.close()
+
+print("Результат в файле:", filename)
 
