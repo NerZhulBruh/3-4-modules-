dbd5ed96ada7fa055ee75f14713f3f77594160ae
NerZhulBruh <116255135+NerZhulBruh@users.noreply.github.com>
2023-06-21 09:50:33 +0300

    Update dz_emoji.py

--- dz_emoji.py
+++ dz_emoji.py
@@ -3,15 +3,19 @@ from bs4 import BeautifulSoup
 
 url = 'https://emojipedia.org/search/'
 
-categories = ["nature", "music", "science"]
+categories = ["nature", "music", "science", "food", "sports"]
 
 for category in categories:
     params = {'q': category}
     response = requests.get(url, params=params)
     soup = BeautifulSoup(response.text, 'html.parser')
     elements = soup.find('ol', class_='search-results')
-    items = elements.find_all('a')
-    all_emojis = [item.text for item in items]
-    quantity = len(all_emojis)
-    print(f' {category} содержит {quantity} эмоджи.')
-    print(all_emojis)
+
+    if elements:
+        items = elements.find_all('a')
+        all_emojis = [item.text for item in items]
+        quantity = len(all_emojis)
+        print('{} содержит {} эмоджи.'.format(category, quantity))
+        print(all_emojis)
+    else:
+        print('На странице не найдены эмоджи для категории {}.'.format(category))
